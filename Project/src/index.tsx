ccad2997a05a718f677f04aa653822885dc1d89f
Henrique Falconer <henrique.falconer@polijunior.com.br>
2020-11-04 15:39:07 -0300

    add production test with __DEV__ = false

--- Project/src/index.tsx
+++ Project/src/index.tsx
@@ -4,6 +4,9 @@ import CalculatorScreen from './CalculatorScreen';
 import { OrientionProvider } from './hooks/useOrientation';
 import { CalculatorProvider } from './hooks/useCalculator';
 
+// Descomente para testar o modo de produção (gera um erro, mas funciona):
+// __DEV__ = false;
+
 // Impedir que os console.logs rodem na produção   
 if (!__DEV__) console.log = () => {};
 
