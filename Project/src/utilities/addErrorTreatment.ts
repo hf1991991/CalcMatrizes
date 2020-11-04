133786f36676ab601e572c37705e4a021cbe0904
Henrique Falconer <henrique.falconer@polijunior.com.br>
2020-11-04 15:34:00 -0300

    add production alert to addErrorTreatment

--- Project/src/utilities/addErrorTreatment.ts
+++ Project/src/utilities/addErrorTreatment.ts
@@ -1,8 +1,13 @@
+import { Alert } from "react-native";
+
 const addErrorTreatment = <T>(funct: () => T, defaultValue: T) => {
     try {
         return funct();
     } catch (e) {
-        alert(e);
+        Alert.alert(
+            "Erro de cálculo",
+            __DEV__ ? e : "Calculadora tentou resolver uma expressão muito complicada."
+        );
         return defaultValue;
     }
 }
